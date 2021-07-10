ea1123bd32e5f782dbf95878d8ec76f8c7f0531f
junaid8301 <85234771+junaid8301@users.noreply.github.com>
2021-07-10 15:51:35 +0530

    Add files via upload

---
+++ Pig.js
@@ -0,0 +1,13 @@
+class Pig extends BaseClass {
+  constructor(x, y){
+    super(x,y,50,50);
+    this.image = loadImage("sprites/enemy.png");
+  }
+display()
+{
+  if(this.body.speed<4){
+    super.display()
+  }else{
+    World.remove(world,this.body);
+  }
+}};
